ff62e71daef28b7bd5332354f7b9ebf13e4e9c8f
Codey Pinero <codey.pinero@gmail.com>
2019-03-28 04:14:53 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -3,7 +3,7 @@ def run_guessing_game
   puts "Guess a number between 1 and 6."
   input = gets.chomp
 
-  while input != "exit"
+  while input != "exit" do
 
   if input == computer_output.to_s
     puts "You guessed the correct number!"
