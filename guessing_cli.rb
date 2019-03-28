cb8c2e729a7ba74c8c1027f1ea686b8600ec2c26
Codey Pinero <codey.pinero@gmail.com>
2019-03-28 04:05:09 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -3,7 +3,7 @@ def run_guessing_game
   puts "Guess a number between 1 and 6."
   input = gets.chomp
 
-  while guess
+  while input
   if input == "exit"
     break
   elsif input == rand(1..6).to_s
